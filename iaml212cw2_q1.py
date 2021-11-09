d483bade96b31a05eb7e6cfed44e70e945fa22d2
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2021-11-09 21:50:15 +0000

    continuing

--- iaml212cw2_q1.py
+++ iaml212cw2_q1.py
@@ -22,13 +22,15 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, r2_score, mean_squared_error
+from sklearn.decomposition import PCA
 from iaml_cw2_helpers import *
 from iaml212cw2_my_helpers import *
 
-print_versions()
+
 X, Y = load_Q1_dataset()
-# print(f"X: {X.shape}, Y: {Y.shape}")
+
 Xtrn = X[100:,:]; Ytrn = Y[100:] # training set
+# print(Xtrn.shape, Ytrn.shape)
 Xtst = X[0:100,:]; Ytst = Y[0:100] # testing set
 #<----
 
@@ -48,30 +50,106 @@ def iaml212cw2_q1_1():
         ax.set(ylabel='frequency')
         ax.grid()
         # ax.label_outer()
-
+    plt.savefig("results/1_1.png")
     plt.show()
 # iaml212cw2_q1_1()   # comment this out when you run the function
 
 # Q1.2
-# def iaml212cw2_q1_2():
-#     for i in range(9):
-
+def iaml212cw2_q1_2():
+    for i in range(9):
+        print(np.corrcoef(Xtrn[:,i], Ytrn))
 # iaml212cw2_q1_2()   # comment this out when you run the function
 #
-# # Q1.3
+# [[1.        0.4911759]
+#  [0.4911759 1.       ]]
+# [[1.        0.0874059]
+#  [0.0874059 1.       ]]
+# [[1.         0.22728719]
+#  [0.22728719 1.        ]]
+# [[1.         0.20736605]
+#  [0.20736605 1.        ]]
+# [[1.         0.10772035]
+#  [0.10772035 1.        ]]
+# [[1.        0.1856714]
+#  [0.1856714 1.       ]]
+# [[1.         0.07626074]
+#  [0.07626074 1.        ]]
+# [[1.         0.30445377]
+#  [0.30445377 1.        ]]
+# [[1.         0.24034733]
+#  [0.24034733 1.        ]]
+
+# Q1.3
 # def iaml212cw2_q1_3():
-# #
-# # iaml212cw2_q1_3()   # comment this out when you run the function
+
+        # desribed in Assignment_2.pdf
+
+ # iaml212cw2_q1_3()   # comment this out when you run the function
 #
-# # Q1.4
-# def iaml212cw2_q1_4():
-# #
-# # iaml212cw2_q1_4()   # comment this out when you run the function
+# Q1.4
+def iaml212cw2_q1_4():
+    samVar = np.zeros((9,2)) #initialising numpy array to store the unbiased smaple variance
+    # print(samVar)
+    for i in range(9):
+        #computing unbiased sample variance of each attribute
+        vari = np.array([np.var(Xtrn[:,i], axis=0, ddof=1),str(i)])
+        samVar[i]=vari
+    samVar = samVar[samVar[:,0].argsort()[::-1]] #sorting in descending order
+    print(samVar)
+    print("The unbiased sample variance of each attribute are")
+    # print(samVar[:,0])
+
+    #Q1.4.1 plot
+    plt.plot(samVar[:,0], label="Varaince explained by each of the Attributes")
+    plt.title("Varaince explained by each of the Attributes")
+    plt.xlabel("Attributes")
+    plt.ylabel("Explained Variance")
+    plt.xticks(np.arange(9), samVar[:,1].astype(str))
+    plt.legend()
+    plt.savefig("results/1_4_1.png")
+    plt.show()
+
+    sum = samVar.sum(axis=0)[0]
+    cumsamVar = np.cumsum(samVar[:,0]/sum)
+    print(f"Cumulative explained variance ratio of each Attributes are {cumsamVar}")
+    #Q1.4.2 plot
+    plt.plot(cumsamVar, label="cumulative explained variance ratio")
+    plt.title("cumulative explained variance ratio")
+    plt.xlabel("Attributes")
+    plt.ylabel("Cumulative Explained Variance")
+    plt.xticks(np.arange(9), samVar[:,1].astype(str))
+    plt.legend()
+    plt.savefig("results/1_4_2.png")
+    plt.show()
+
+iaml212cw2_q1_4()   # comment this out when you run the function
 #
 # # Q1.5
-# def iaml212cw2_q1_5():
-# #
-# # iaml212cw2_q1_5()   # comment this out when you run the function
+def iaml212cw2_q1_5():
+    pca = PCA().fit(Xtrn)
+    # print(pca)
+    # print(pca.explained_variance_)
+    # print(np.cumsum(pca.explained_variance_ratio_))
+
+    plt.plot(pca.explained_variance_, label="Varaince explained by each of the Attributes")
+    plt.title("Varaince explained by each of the Attributes")
+    plt.xlabel("Attributes")
+    plt.ylabel("Explained Variance")
+    # plt.xticks(np.arange(9), samVar[:,1].astype(str))
+    plt.legend()
+    plt.savefig("results/1_5_1.png")
+    plt.show()
+
+    print(f"cumsumVar is {np.cumsum(pca.explained_variance_ratio_)}")
+    plt.plot(np.cumsum(pca.explained_variance_ratio_), label="cumulative explained variance ratio")
+    plt.title("cumulative explained variance ratio")
+    plt.xlabel("Attributes")
+    plt.ylabel("Cumulative Explained Variance")
+    # plt.xticks(np.arange(9), samVar[:,1].astype(str))
+    plt.legend()
+    plt.savefig("results/1_5_2.png")
+    plt.show()
+iaml212cw2_q1_5()   # comment this out when you run the function
 #
 # # Q1.6
 # def iaml212cw2_q1_6():
--- iaml212cw2_q2.py
+++ iaml212cw2_q2.py
@@ -18,52 +18,69 @@ import pandas as pd
 import scipy
 import matplotlib.pyplot as plt
 import seaborn as sns
-from iaml212cw2_helpers import *
+from iaml_cw2_helpers import *
 # from iaml212cw2_my_helpers import *
+Xtrn_org, Ytrn_org, Xtst_org, Ytst_org = load_Q2_dataset()
 
-# pd.<----
+Xtrn = Xtrn_org / 255.0
+Xtst = Xtst_org / 255.0
+Ytrn = Ytrn_org - 1
+Ytst = Ytst_org - 1
+Xmean = np.mean(Xtrn, axis = 0)
+Xtrn_m = Xtrn - Xmean; Xtst_m = Xtst - Xmean # Mean normalised versions
+#<----
 
 # Q2.1
 def iaml212cw2_q2_1():
-#
-# iaml212cw2_q2_1()   # comment this out when you run the function
+    print('Number of instances: {}, number of attributes: {}'.format(Xtrn.shape[0], Xtrn.shape[1]))
+    Xtrndf = pd.DataFrame(Xtrn)
+    print(np.max(Xtrn), np.min(Xtrn), np.mean(Xtrn), np.std(Xtrn))
+    # print(Xtrndf.describe())
+
+
+    print('Number of instances: {}, number of attributes: {}'.format(Xtst.shape[0], Xtst.shape[1]))
+    Xtstdf = pd.DataFrame(Xtst)
+    print(np.max(Xtst), np.min(Xtst), np.mean(Xtst), np.std(Xtst))
+    # print(Xtstdf.describe())
+    # Xtst.describe()
+iaml212cw2_q2_1()   # comment this out when you run the function
 
 # Q2.2
-def iaml212cw2_q2_2():
+# def iaml212cw2_q2_2():
 #
 # iaml212cw2_q2_2()   # comment this out when you run the function
 
 # Q2.3
-def iaml212cw2_q2_3():
+# def iaml212cw2_q2_3():
 #
 # iaml212cw2_q2_3()   # comment this out when you run the function
 
 # Q2.4
-def iaml212cw2_q2_4():
+# def iaml212cw2_q2_4():
 #
 # iaml212cw2_q2_4()   # comment this out when you run the function
 
 # Q2.5
-def iaml212cw2_q2_5():
+# def iaml212cw2_q2_5():
 #
 # iaml212cw2_q2_5()   # comment this out when you run the function
 
 # Q2.6
-def iaml212cw2_q2_6():
+# def iaml212cw2_q2_6():
 #
 # iaml212cw2_q2_6()   # comment this out when you run the function
 
 # Q2.7
-def iaml212cw2_q2_7():
+# def iaml212cw2_q2_7():
 #
 # iaml212cw2_q2_7()   # comment this out when you run the function
 
 # Q2.8
-def iaml212cw2_q2_8():
+# def iaml212cw2_q2_8():
 #
 # iaml212cw2_q2_8()   # comment this out when you run the function
 
 # Q2.9
-def iaml212cw2_q2_9():
+# def iaml212cw2_q2_9():
 #
 # iaml212cw2_q2_9()   # comment this out when you run the function
